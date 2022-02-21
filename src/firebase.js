9ff7b0c64b0594103dd6f50d8c6dcafb90ae0a32
plnyakan <plnyakan@student.wethinkcode.co.za>
2022-02-21 21:58:02 +0200

    building the header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5zkCGfOV-3A0Jce41Y0h1YkgIUak8wT0",
+    authDomain: "slack-clone-1a4e2.firebaseapp.com",
+    projectId: "slack-clone-1a4e2",
+    storageBucket: "slack-clone-1a4e2.appspot.com",
+    messagingSenderId: "1068663303530",
+    appId: "1:1068663303530:web:daecb74db4a73f5d9d8913"
+  };
